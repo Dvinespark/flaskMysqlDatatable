cd0568f8dc66f827fdd1e4a1170455ffe7fd34e0
Shubhas <nimtsubash@gmail.com>
2021-11-10 11:54:56 -0500

    js file separated from html

--- static/js/user.js
+++ static/js/user.js
@@ -1,38 +1,94 @@
-$(document).ready(function(){
+function dataTable_renderer(real_data){
+    $('#user_dataTable').DataTable( {
+                data: real_data,
+               "columnDefs": [
+                {
+                    "targets": 0,
+                    "visible": false,
+                    "searchable": false
+                },
+                   {
+                       "targets": 9,
+                       "render": function(data, type, row) {
+                           return "<a href='static/files/" + data + "'> Click here! </a>";
+                       }
+                   }
+            ],
+                columns: [
+                    { title: "id" },
+                    { title: "First Name" },
+                    { title: "Last Name" },
+                    { title: "Email" },
+                    { title: "Phone number" },
+                    { title: "Status" },
+                    { title: "Availability" },
+                    { title: "Skill" },
+                    { title: "Years" },
+                    { title: "Resume" }
+                    ]
+                } );
+}
+
+function handler(data){
+    dataTable_renderer(data);
     $('#addUserModal').on('show.bs.modal', function (event) {
-        $('#cancel_button').on('click', function (e) {
-            $('#addUserModal').modal('toggle');
-            return false;
+            $('#cancel_button').on('click', function (e) {
+                $('#addUserModal').modal('toggle');
+                return false;
+            });
+            $('#addRegister_button').on('click', function (e){
+                // Create Applicant
+                let params = {};
+                $.each($('#register_employee').serializeArray(), function (index, value) {
+                    params[value.name] = params[value.name] ? params[value.name] || value.value : value.value;
+                });
+                let form_data = new FormData();
+                for(let key in params){
+                    form_data.append(key, params[key]);
+                }
+                form_data.append('resume', $("#resume")[0].files[0]);
+                console.log(form_data)
+                $.ajax({
+                    type: 'post',
+                    url: "create",
+                    data: form_data,
+                    processData: false,
+                    contentType: false,
+                    cache: false,
+                    success: function (response) {
+                        console.log(response);
+                        if(response.status_code){
+                            window.location.href = response.url;
+                        }
+                    }
+                });
+                e.preventDefault();
+            });
+
         });
-        $('#addRegister_button').on('click', function (e){
-            // Create Applicant
+    $('#searchbar_button').on('click', function (e){
             let params = {};
-            $.each($('#register_employee').serializeArray(), function (index, value) {
+            $.each($('#search_form').serializeArray(), function (index, value) {
                 params[value.name] = params[value.name] ? params[value.name] || value.value : value.value;
             });
-            let form_data = new FormData();
+            let url = "/filter?"
             for(let key in params){
-                form_data.append(key, params[key]);
+                url+=key+ "=" + params[key] +"&";
             }
-            form_data.append('resume', $("#resume")[0].files[0]);
-            console.log(form_data)
             $.ajax({
-                type: 'post',
-                url: "create",
-                data: form_data,
+                type: 'get',
+                url: url,
                 processData: false,
                 contentType: false,
                 cache: false,
                 success: function (response) {
-                    console.log(response);
-                    if(response.status_code){
-                        window.location.href = response.url;
-                    }
+                    let datatable = $("#user_dataTable").DataTable();
+                    let json_data = response["data"].replaceAll("&#34;", '"');
+                    let real_data = $.parseJSON(json_data);
+                    datatable.clear().rows.add(real_data).draw();
                 }
             });
             e.preventDefault();
         });
 
-    });
-
-});
+}
